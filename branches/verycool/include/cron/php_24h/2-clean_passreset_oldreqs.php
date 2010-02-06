77c17a2b3231e8b17ca2753b61a5c4146fd7403d
liruqi <liruqi@35712a82-d2b3-11de-9aee-2b7272db14c4>
2010-02-06 17:01:36 +0000

    先删掉所有文件

--- branches/verycool/include/cron/php_24h/2-clean_passreset_oldreqs.php
+++
@@ -1,5 +0,0 @@
-<?php
-	
-	$db->query('UPDATE users SET pass_reset_key="", pass_reset_valid=0 WHERE pass_reset_key<>"" AND pass_reset_valid<"'.(time()-2*24*60*60).'" ');
-	
-?>
--- branches/verycool/include/pages/pg_mobileversion.php
+++
@@ -1,7 +0,0 @@
-<?php
-	
-	$this->title	= $this->lang('mobipage_title');
-	
-	$html	.= $this->lang('mobipage_text');
-	
-?>
